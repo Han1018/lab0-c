8072c39065943e79b754d76847707105560d7de9
ZongHan <zonghan.ai12@nycu.edu.tw>
2024-02-26 22:01:47 +0800

    Remove an element from tail of queue

--- queue.c
+++ queue.c
@@ -102,7 +102,24 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (list_empty(head))
+        return NULL;
+
+    element_t *entry = list_entry(head->prev, element_t, list);
+    if (entry->value != NULL) {
+        strncpy(sp, entry->value, bufsize);
+        free(entry->value);
+        entry->value = NULL;
+    }
+
+    // check if head is the only element
+    if (head->next == head->prev) {
+        list_del_init(head->prev);
+        return entry;
+    } else {
+        list_del(head->prev);
+        return entry;
+    }
 }
 
 /* Return number of elements in queue */
