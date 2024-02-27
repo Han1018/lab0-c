846932dce7d88f805f53b1528bd60a0c84af9054
ZongHan <zonghan.ai12@nycu.edu.tw>
2024-02-28 07:15:59 +0800

    Reverse elements in queue

--- queue.c
+++ queue.c
@@ -207,7 +207,20 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (list_empty(head) || list_is_singular(head))
+        return;
+
+    struct list_head *end = head->prev;
+    struct list_head *current = head->next;
+    while (current != end) {
+        struct list_head *next = current->next;
+        list_move(current, head);
+        current = next;
+    }
+    list_move(current, head);  // add last node
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
